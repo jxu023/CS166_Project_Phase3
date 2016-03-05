0b1cfa05d37b2f3b8495021c1ee7bf23aa660a1f
Jacob Xu <jxu023@well.cs.ucr.edu>
2016-03-04 17:07:54 -0800

    ejgoweigj

--- foo.sql
+++
@@ -1,9 +0,0 @@
-SELECT u1.login AS Contacts, u1.status AS Status
-FROM (
-    SELECT con.list_member
-    FROM USER_LIST_CONTAINS con, USR u
-    WHERE u.login = 'Norma'
-    AND u.contact_list = con.list_id
-) AS list, USR u1
-WHERE list.list_member = u1.login
-
